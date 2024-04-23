b90fc499c9278cf69659cc2145e5975c6573858f
mvacoimbra <mv27.marcosvinicius@gmail.com>
2024-04-23 15:07:53 -0300

    fix: fix hero section style

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -6,7 +6,7 @@ export default function Hero() {
     <Section
       id='home'
       // className='h-[100svh] w-full py-3 px-[100px] relative max-sm:px-[20px] overflow-hidden'
-      sectionClasses='h-[95vh]'
+      sectionClasses='h-[99vh]'
     >
       <div className='text-white flex flex-col justify-center items-center h-full'>
         <h2 className='text-6xl text-center text-green-glow max-sm:text-4xl'>
@@ -22,16 +22,14 @@ export default function Hero() {
         className={`w-screen h-1/2 absolute bottom-0 left-0 pointer-events-none z-0`}
       />
       <div className='absolute left-0 top-0 w-full h-screen -z-50'>
-        <div className='h-[200vh] relative'>
-          <figure className='fixed'>
-            <div className='bg-gradient-to-b to-20% from-black to-transparent w-full h-full absolute top-0 left-0' />
-            <div className='bg-[#1EFCA3] w-full h-full mix-blend-color absolute top-0 left-0' />
-            <img
-              className='w-screen h-screen object-cover'
-              src='https://res.cloudinary.com/mvacoimbra/image/upload/v1713886412/mvacoimbra-porfolio/synth-bg-3_doqmzj.gif'
-            />
-          </figure>
-        </div>
+        <figure className='fixed'>
+          <div className='bg-gradient-to-b to-20% from-black to-transparent w-full h-full absolute top-0 left-0' />
+          <div className='bg-[#1EFCA3] w-full h-full mix-blend-color absolute top-0 left-0' />
+          <img
+            className='w-screen h-screen object-cover'
+            src='https://res.cloudinary.com/mvacoimbra/image/upload/v1713886412/mvacoimbra-porfolio/synth-bg-3_doqmzj.gif'
+          />
+        </figure>
       </div>
     </Section>
   );
