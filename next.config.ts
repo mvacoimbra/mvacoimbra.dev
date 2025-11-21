dbcece457c3588741629c16ccc3f7e3573f9a78f
mvacoimbra <mvacoimbra.dev@gmail.com>
2025-11-21 10:57:44 -0300

    feat: add tailwind and shadcn

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import { withPayload } from '@payloadcms/next/withPayload'
+import type { NextConfig } from 'next'
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig: NextConfig = {
   // Your Next.js config here
   webpack: (webpackConfig) => {
     webpackConfig.resolve.extensionAlias = {
@@ -9,7 +9,6 @@ const nextConfig = {
       '.js': ['.ts', '.tsx', '.js', '.jsx'],
       '.mjs': ['.mts', '.mjs'],
     }
-
     return webpackConfig
   },
 }
