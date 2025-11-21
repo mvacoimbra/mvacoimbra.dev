3d540e8aa0cabffaf9b5fe9120f6e67b552f20e7
mvacoimbra <mvacoimbra.dev@gmail.com>
2025-11-21 10:34:49 -0300

    refactor: reorganize project

--- app/(frontend)/page.tsx
+++ app/(frontend)/page.tsx
@@ -1,15 +1,13 @@
+import { fileURLToPath } from 'node:url'
+import getPayloadConfig from '@payload-config'
 import { headers as getHeaders } from 'next/headers.js'
 import Image from 'next/image'
 import { getPayload } from 'payload'
-import React from 'react'
-import { fileURLToPath } from 'url'
-
-import config from '@/payload.config'
 import './styles.css'
 
 export default async function HomePage() {
   const headers = await getHeaders()
-  const payloadConfig = await config
+  const payloadConfig = await getPayloadConfig
   const payload = await getPayload({ config: payloadConfig })
   const { user } = await payload.auth({ headers })
 
